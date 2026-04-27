4c0adba5a463f2669bd8bf7663fb6f8cc9074799
prplhd <dimas.kovalenko2000@mail.ru>
2026-04-27 18:30:08 +0700

    feat(auth): add sign-in page route

---
+++ src/main/java/ru/prplhd/weather/controller/AuthController.java
@@ -0,0 +1,15 @@
+package ru.prplhd.weather.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/sign-in")
+    public String signInPage() {
+        return "sign-in";
+    }
+}
