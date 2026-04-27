779a8a3a6873a9156a14a28e0aa844ab15178361
prplhd <dimas.kovalenko2000@mail.ru>
2026-04-27 17:15:47 +0700

    build(deps): add Spring MVC, Thymeleaf and Servlet dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("war")
 }
 
 group = "ru.prplhd"
@@ -13,6 +14,12 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation(platform("org.springframework:spring-framework-bom:7.0.7"))
+    implementation("org.springframework:spring-webmvc")
+
+    implementation("org.thymeleaf:thymeleaf-spring6:3.1.5.RELEASE")
+    compileOnly("jakarta.servlet:jakarta.servlet-api:6.1.0")
 }
 
 tasks.test {
